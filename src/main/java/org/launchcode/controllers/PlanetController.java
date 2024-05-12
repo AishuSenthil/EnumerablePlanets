a8ac53cb8195ef41a04a33e6dc026793cee7b4bc
AishuSenthil <124016737+AishuSenthil@users.noreply.github.com>
2024-05-12 15:25:23 -0500

    Enums

--- src/main/java/org/launchcode/controllers/PlanetController.java
+++ src/main/java/org/launchcode/controllers/PlanetController.java
@@ -1,13 +1,15 @@
 package org.launchcode.controllers;
 
+import org.launchcode.Data.Planets;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class PlanetController {
-
-    @GetMapping
-    public String displayIndex() {
+    @GetMapping()
+    public String displayIndex(Model model) {
+        model.addAttribute("planets", Planets.values());
         return "index";
     }
 }
